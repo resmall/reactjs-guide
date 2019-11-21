e71f106df95337cd66e9e2763a8278f7282cbfb7
Tiago R Lammers <trlammers@gmail.com>
2019-11-21 16:18:22 -0300

    React hooks example, functional components with state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: "Tiago", age: "31"},
       { name: "Tainara", age: "24"},
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const [ otherState, setOtherState ] = useState({otherstate: 'other state'})
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () =>{
+    setPersonsState({
       persons: [
         { name: "Tiago", age: "31"},
         { name: "Tainara", age: "22"},
@@ -20,17 +24,16 @@ class App extends Component {
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Very cool course</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        {this.state.persons.map(person => 
-          <Person key={person.name} name={person.name} age={person.age} />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Very cool course</h1>
+      <button onClick={switchNameHandler}>Switch name</button>
+      {personsState.persons.map(person => 
+        <Person key={person.name} name={person.name} age={person.age} />
+      )}
+    </div>
+  );
 }
 
-export default App;
+export default app;
+
