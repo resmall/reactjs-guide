017249f4d886d051439bb2bcbc10f44d3a5d5ec6
Tiago R Lammers <trlammers@gmail.com>
2019-11-21 15:56:16 -0300

    Changing states

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Tiago", age: "31"},
+      { name: "Tainara", age: "24"},
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Tiago", age: "31"},
+        { name: "Tainara", age: "22"},
+        { name: "José", age: "0"},
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Very cool course</h1>
-        <Person name="Tiago" age="31" />
-        <Person name="Tainara" age="24">My hobbies are: fishing</Person>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        {this.state.persons.map(person => 
+          <Person key={person.name} name={person.name} age={person.age} />
+        )}
       </div>
     );
   }
